81c1fc37e46bffd2be0547879531a9b90a4b0d0a
“Diego <your@email.com>
2021-08-24 18:51:28 +0200

    header change

--- components/Header.js
+++ components/Header.js
@@ -57,15 +57,20 @@ const selectionRange = {
             />
             </div>
             {/* Middle */}
-            <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
-                <input 
-                value={searchInput}
-                onChange={(e) => setSearchInput(e.target.value)} 
-                type="text" 
-                placeholder={placeholder || "Start your search"}
-                className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
-                />
-                <SearchIcon className="hidden md:inline-flex h-8 w-8 bg-red-400 text-white rounded-full p-2 cursor pointer md:mx-2"/>
+            <div>
+                <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+                    <input 
+                    value={searchInput}
+                    onChange={(e) => setSearchInput(e.target.value)} 
+                    type="text" 
+                    placeholder={placeholder || "Start your search"}
+                    className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+                    />
+                    <SearchIcon className="hidden md:inline-flex h-8 w-8 bg-red-400 text-white rounded-full p-2 cursor pointer md:mx-2"/>
+                </div>
+                <div>
+                    <h3>Built with ❤️   by Diego Zito</h3>
+                </div>
             </div>
             {/* Right */}
             <div className="flex items-center space-x-4 justify-end text-gray-500">
