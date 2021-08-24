369f8d18a442113965677968ab8a896e18cc8bb6
“Diego <your@email.com>
2021-08-24 18:43:23 +0200

    mapbox functionality added

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     images: {
-        domains: ['links.papareact.com', 'www.unsplash.com']
+        domains: ['links.papareact.com', 'www.unsplash.com'],
+    },
+    env: {
+        mapbox_key: 'pk.eyJ1IjoiZGllZ296aXRvMSIsImEiOiJja3NxOHJ4aWswYXczMnF0aGIxa2o1N255In0.4F1_R_Dp8Z0EL2PDxgRkKQ'
     }
-}
+};
